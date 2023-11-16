96465392a75c4de3b2096b5e92d8ea4ec26f058a
Ragnar Thor <ragnarthor102003@gmail.com>
2023-11-16 00:37:35 +0000

    smá breytingar við my masterpiece

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 let audioContext = new (window.AudioContext || window.webkitAudioContext)();
 let audioBuffer;
 
-// Function to load the audio file
 function loadAudio() {
     fetch('sounds/BRUH (MEME) Sound Effect.mp3')
         .then(response => response.arrayBuffer())
@@ -11,7 +10,6 @@ function loadAudio() {
         });
 }
 
-// Call this function when the script loads to ensure the audio is ready to play
 loadAudio();
 
 let count = 0;
@@ -20,14 +18,14 @@ document.getElementById("increaseButton").onclick = function() {
     count++;
     document.getElementById("countLabel").innerHTML = count;
 
-    // Play the sound using Web Audio API
+    //Spila hljóðið með að nota Web Audio API
     let source = audioContext.createBufferSource();
     source.buffer = audioBuffer;
     source.connect(audioContext.destination);
     source.start();
 };
 
-// Ensure that the audio context is resumed on user interaction due to browser restrictions
+//passar að hljóðið heldur áfram
 document.addEventListener('click', function() {
     if (audioContext.state === 'suspended') {
         audioContext.resume();
